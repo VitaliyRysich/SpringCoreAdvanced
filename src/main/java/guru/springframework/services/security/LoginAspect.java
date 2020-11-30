1406198093af61de378f84c5ef436e56691edca2
Vitalii <vitaliy.rysich@gmail.com>
2020-11-30 22:06:18 +0100

    Custom Events

--- src/main/java/guru/springframework/services/security/LoginAspect.java
+++ src/main/java/guru/springframework/services/security/LoginAspect.java
@@ -1,12 +1,20 @@
 package guru.springframework.services.security;
 
 import org.aspectj.lang.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoginAspect {
+
+    private LoginFailureEventPublisher publisher;
+
+    @Autowired
+    public void setPublisher(LoginFailureEventPublisher publisher) {
+        this.publisher = publisher;
+    }
 
     @Pointcut("execution(* org.springframework.security.authentication.AuthenticationProvider.authenticate(..))")
     public void doAuthenticate(){
@@ -27,5 +35,7 @@ public class LoginAspect {
     public void logAuthenticationException(Authentication authentication){
         String userDetails = (String) authentication.getPrincipal();
         System.out.println("Login failed for user: " + userDetails);
+
+        publisher.publish(new LoginFailureEvent(authentication));
     }
 }
