451e24fec9b4447a8410e863ff50dcfe81532fe9
Vitalii <vitaliy.rysich@gmail.com>
2020-12-01 17:19:14 +0100

    Solve h2-console problem

--- src/main/java/guru/springframework/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/config/SpringSecConfig.java
@@ -33,6 +33,8 @@ public class SpringSecConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().ignoringAntMatchers("/h2-console").disable()
+                .headers().frameOptions().disable()
+                .and()
                 .authorizeRequests().antMatchers("/**/favicon.ico") .permitAll()
                 .and().authorizeRequests().antMatchers("/product/**").permitAll()
                 .and().authorizeRequests().antMatchers("/webjars/**").permitAll()
